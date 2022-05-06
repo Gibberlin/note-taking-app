2a528e0e1d2009b8791a9476507610fdfc9e1c2c
Gibberlin <syedeyashin@gmail.com>
2022-05-06 09:09:10 +0530

    scafolded the app and made the navbar

---
+++ src/componets/Nav.js
@@ -0,0 +1,22 @@
+import React from 'react'
+import "./style/nav.css"
+
+export const Nav = () => {
+  return (
+    <div className='navbar'>
+    <ul>
+    <li className='logo'>logo</li>
+    <span className='nav-links'>
+    <li>Home</li>
+    <li>About</li>
+    <li>Contact</li>
+    </span>
+    </ul>
+    
+    
+    
+    
+    </div>
+  )
+}
+export default Nav
